7566939adacb28a1fccc83ce6a473c59ba5494cc
sayemaa <sayema990@gmail.com>
2022-05-01 01:29:33 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Pages/Shared/Header/Header';
+import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Authentication/Login/Login';
+import Blogs from './Pages/Blogs/Blogs';
 
 function App() {
     return (
         <div className="App">
-            <h1>Welcome to my Warehouse management website</h1>
+            <Header></Header>
+            <Routes>
+                <Route path='/' element={<Home></Home>}></Route>
+                <Route path='/home' element={<Home></Home>}></Route>
+                <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+                <Route path='/login' element={<Login></Login>}></Route>
+            </Routes>
         </div>
     );
 }
---
+++ src/Pages/Blogs/Blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+    return (
+        <div>
+            <h2>Frequently Asked Questions</h2>
+        </div>
+    );
+};
+
+export default Blogs;
